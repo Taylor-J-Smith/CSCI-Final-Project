e94723129afc9d027225da785a0c9c06eec9be45
tjs <taylor.smith1@uoit.net>
2017-04-17 16:49:11 -0400

    Initial pthreads implementation

--- main.c
+++ main.c
@@ -3,8 +3,19 @@
 #include <stdlib.h>  // atoi, malloc, free, exit, NULL
 #include <stdbool.h> // bool
 
+#include <pthread.h>
+
 #define DEBUG 0
 #define VERBOSE 0
+
+#define NUM_THREADS 4
+
+long num;
+double sqrt_num;
+
+bool* is_prime;
+
+void * prime_seive_multiple(void* args);
 
 /*
  * Input should be a single number
@@ -20,8 +31,8 @@ int main(int argc, char *argv[])
     /*
      * Take input number, convert to an integer
      */
-    long num = atol(argv[1]);
-    double sqrt_num = sqrt(num);
+    num = atol(argv[1]);
+    sqrt_num = sqrt(num);
 
     if( DEBUG )
         printf("N: %ld, S: %.2f\n", num, sqrt_num);
@@ -30,7 +41,7 @@ int main(int argc, char *argv[])
 
     ///////////////////////////// INIT /////////////////////////////////////////
 
-    bool* is_prime = malloc (sizeof(bool) * num);
+    is_prime = malloc (sizeof(bool) * num);
 
     if(is_prime == NULL)
     {
@@ -54,24 +65,21 @@ int main(int argc, char *argv[])
 
     // END INIT
 
-    // SEIVE
+    pthread_t threads[NUM_THREADS];
 
-    for(long jj = 2; jj < sqrt_num; jj++)
+    for(int jj = 0; jj < NUM_THREADS; jj++)
     {
-        if( !is_prime[jj] )
-            continue;
-
-        if( DEBUG )
-            printf("loop itteration: %ld\n", jj);
-
-        for(long kk = jj*jj; kk < num; kk+=jj)
-        {
-            is_prime[kk] = false;
-        }
-
+        pthread_create(&threads[jj], NULL, &prime_seive_multiple, (void *) jj );
     }
 
+    // SEIVE
+
     // END SEIVE
+
+    for(int jj = 0; jj < NUM_THREADS; jj++)
+    {
+        pthread_join(threads[jj], NULL);
+    }
 
     // OUTPUT
 
@@ -91,4 +99,30 @@ int main(int argc, char *argv[])
     free(is_prime);
 
     return 0;
+}
+
+void * prime_seive_multiple(void* args)
+{
+    int id = (int) args;
+
+    // printf("Thread %d\n", id);
+
+
+    for(long jj = 2 + id; jj < sqrt_num; jj+= 4)
+    {
+        if( !is_prime[jj] )
+            continue;
+
+        if( DEBUG )
+            printf("loop itteration: %ld\n", jj);
+
+        for(long kk = jj*jj; kk < num; kk+=jj)
+        {
+            is_prime[kk] = false;
+        }
+
+    }
+
+
+    pthread_exit(0);
 }
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 compiler = gcc
 args = -Wall -Wextra -std=c99
-libs = -lm
+libs = -lm -lpthread
 
 all: run
 
